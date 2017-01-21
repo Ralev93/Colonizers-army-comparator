85672065f4bc950b82e61c6fa7eb4a6b693c4b78
Ralev93 <Ralev93@users.noreply.github.com>
2017-01-22 00:13:15 +0200

    Add files via upload

---
+++ ArmyComparator/calc2.js
@@ -0,0 +1,139 @@
+"use strict";
+
+function charecteristics (dmg, armor, cost) {
+	return {
+		'dmg': dmg, 'armor': armor, 'cost': cost
+	}
+}
+
+
+function NotDataError(message) {
+    this.name = "NotDataError";
+    this.message = (message || "");
+}
+NotDataError.prototype = Error.prototype;
+
+
+String.prototype.supplant = function (o) {
+    return this.replace(/{([^{}]*)}/g,
+        function (a, b) {
+            var r = o[b];
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+    );
+};
+
+
+String.prototype.capitalizeFirstLetter = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+
+function Unit(name, fire_rate, supplies_needed, levels) {
+
+	var NotDataError_MSG = "No data for {unit_name}, level {level}!";
+	
+	this.getName = function() {
+		return name;
+	};
+
+	this.getFireRate = function() {
+		return fire_rate;
+	};
+
+	this.getSuppliesNeeded = function() {
+		return supplies_needed;
+	};
+
+	this.getLevels = function() {
+		return levels;
+	};
+
+	this.getDamagePerSec = function(level) {
+		try { 
+			return levels[level].dmg / fire_rate; 
+		} catch(e) {
+			throw new NotDataError(NotDataError_MSG.supplant({'unit_name': name, 'level': level}));
+
+		}
+	};
+
+	this.getArmor = function(level) {
+		try { 
+			return levels[level].armor; 
+		} catch(e) {
+			throw new NotDataError(NotDataError_MSG.supplant({'unit_name': name, 'level': level}));
+		}
+	};
+
+}
+
+
+function Troop(Unit, level, count) {
+
+	this.getLevel = function () {
+		return level;
+	};
+
+	this.getCount = function() {
+		return count;
+	};
+
+	this.getArmor = function() {
+		return Unit.getArmor(level);
+	};
+
+	this.getDmgPerSec = function() {
+		return Unit.getDamagePerSec(level)
+	};
+
+} 
+
+
+function Army (troops) {
+
+	this.getDamage = function(){
+		var total = 0;
+		troops.forEach(function (troop) {
+			total += troop.getDmgPerSec() * troop.getCount();
+		})
+		return total;
+	};
+
+	this.getArmor = function () {
+		var total = 0;
+		troops.forEach(function (troop) {
+			total += troop.getArmor() * troop.getCount();
+		})
+		return total;
+	};
+
+	this.compare = function(army2) {
+		
+		try	{
+			var 
+				result = "",  dmg_ratio, armor_ratio, 
+				dmg1 = this.getDamage(), dmg2 = army2.getDamage(),
+				arm1 = this.getArmor(), arm2 = army2.getArmor();
+		} catch (e) {
+			if (e instanceof NotDataError) {
+				return e.message;
+			}else{
+				throw e;
+			}
+		}
+
+		 dmg_ratio = (dmg1 <= dmg2) ? 1 - (dmg1 / dmg2)  : (dmg2 / dmg1) - 1;
+		 armor_ratio = (arm1 <= arm2) ? 1 - (arm1 / arm2) : (arm2 / arm1) - 1;
+
+		result += "Damage " + ((dmg_ratio > 0) ?  "increase: " : "decrease: ") + (dmg_ratio * 100).toFixed(2) + '% <br>';
+		result += "Armor " + ((armor_ratio > 0) ?  "increase: " : "decrease: ") + (armor_ratio * 100).toFixed(2) + '% <br>';
+
+		return result;
+	};
+
+	this.getTroopsNum = function(){
+		return troops.length;
+	};
+
+}
---
+++ ArmyComparator/data.js
@@ -0,0 +1,36 @@
+var Bot = new Unit("Bot", 1.5, 4, {
+    5 : charecteristics(303, 281, 5000),
+    6 : charecteristics(319, 295, 6000)
+});
+
+var Bat = new Unit("Bat", 0.9, 6, {
+    4 : charecteristics(249, 400, 3500),
+    5 : charecteristics(297, 450, 4500),
+    6 : charecteristics(297+71, 510, 5500)
+});
+
+var Levithian = new Unit("Levithian", 2, 5,  {
+    6 : charecteristics(73, 1700, 5000),
+});
+
+var Cruiser = new Unit("Cruiser", 1.5, 20,{
+    3 : charecteristics(323, 4150, 50000),
+    4 : charecteristics(356, 4500, 57500),
+    5 : charecteristics(356+39, 4800, 57500+7500), // 400k xp
+});
+
+var Widow = new Unit("Widow", 2.4, 25, {
+    6 : charecteristics(116, 11550, 195),
+});
+
+var Plasmon = new Unit("Plasmon", 2.5, 25, {
+    3 : charecteristics(538, 6300, 57500),
+    4 : charecteristics(608, 7250, 65000),
+    5 : charecteristics(678, 8000, 70000),
+    6 : charecteristics(748, 8800, 77500),
+});
+
+var Pulsar  = new Unit("Pulsar", 4, 5, {
+    5 : charecteristics(248, 900, 6400),
+    6 : charecteristics(248+62, 1150, 6400+1700),
+});
